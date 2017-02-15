d40d78dfa9c373927ffbac6cf973642a9088b8a1
Takashi SASAKI <takashi316@gmail.com>
2017-02-15 19:11:53 +0900

    support POST method

--- simple-web-server.js
+++ simple-web-server.js
@@ -1,6 +1,31 @@
 var http = require("http")
+var url = require("url");
+
 http.createServer(function(req, res){
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end("Hello");
+
+  if(req.method==="GET"){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var params = url.parse(req.url, true);
+    res.write(JSON.stringify(params));
+    res.write("\n");
+    res.end("Hello GET");
+  }//if
+ 
+  if(req.method==="POST"){
+    var body ="";
+    req.on("data", function(data){
+      body+=data;
+    });
+    req.on("end", function(){
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      var params = url.parse(req.url, true);
+      res.write(JSON.stringify(params));
+      res.write("\n");
+      res.write(body);
+      res.write("\n");
+      res.end("Hello POST");
+    });
+  }//if
+
 }).listen(10080, "127.0.0.1");
 

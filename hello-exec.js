9f9170170902e368cdd6d0a6a865a3f741614f46
Takashi SASAKI <takashi316@gmail.com>
2017-02-16 08:08:27 +0900

    added spawn

--- hello-exec.js
+++ hello-exec.js
@@ -1,5 +1,32 @@
 var exec = require("child_process").exec
+
 exec("date", function(err, stdout, stderr){
+  console.log("--- result of exec 1");
   console.log(stdout);
 });
+
+exec("date -Iseconds", function(err, stdout, stderr){
+  console.log("--- result of exec 2");
+  console.log(stdout);
+});
+
+var spawn = require("child_process").spawn
+var result = spawn("date");
+result.stdout.on("data", function(data){
+  console.log("--- result of spawn 1");
+  console.log(data.toString());
+});
+
+var result = spawn("date", ["-Iseconds"]);
+result.stdout.on("data", function(data){
+  console.log("--- result of spawn 2");
+  console.log(data.toString());
+});
+
+var result = spawn("date", ["-Iseconds"], {shell:true});
+result.stdout.on("data", function(data){
+  console.log("--- result of spawn 3");
+  console.log(data.toString());
+});
+
 
